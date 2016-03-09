1abece5c1fb001547c2ca823c5e5bfa2e1bdbfc1
Brice Wilson <brice@5wilsons.net>
2016-03-08 23:32:13 -0500

    squashed some commits

--- js/app.js
+++
@@ -1,8 +0,0 @@
-function init() {
-    var firstButton = document.getElementById('clickme');
-    firstButton.addEventListener("click", function() {
-        console.log('Hello from the event listener!');
-    })
-}
-
-init();
