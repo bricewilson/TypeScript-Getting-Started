1a362de2646cb34315eb999fd557317013604d78
Brice Wilson <brice@5wilsons.net>
2021-02-16 16:06:18 -0500

    packages for course update

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   entry: './app/app.ts',
   devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
   resolve: {
     extensions: [ '.tsx', '.ts', '.js' ]
   },
