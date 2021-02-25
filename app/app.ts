45fa5c04bb4428a28b2cf9f794df24bd1b7842e1
Brice Wilson <brice@5wilsons.net>
2021-02-25 16:20:55 -0500

    add m3 code

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,15 @@
 function startGame() {
   // starting a new game
-  var messagesElement = document.getElementById('messages');
+
+  let playerName: string = 'Audrey';
+  logPlayer(playerName);
+  
+  let messagesElement = document.getElementById('messages');
+  
   messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
 }
 
+function logPlayer(name) {
+  console.log(`New game starting for player: ${name}`);
+}
 document.getElementById('startGame')!.addEventListener('click', startGame);
