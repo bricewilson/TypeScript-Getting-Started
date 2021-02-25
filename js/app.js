7b13965965aa46bf4b4dc53fc1d6bf7f292162c4
Brice Wilson <brice@5wilsons.net>
2021-02-25 10:45:29 -0500

    recorded demo 5

--- js/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function startGame() {
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
-}
-document.getElementById('startGame').addEventListener('click', startGame);
-//# sourceMappingURL=app.js.map
