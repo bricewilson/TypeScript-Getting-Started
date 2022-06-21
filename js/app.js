15b0242ad7502e1faa027df6ff91447370bac4d7
Anni Parkkila <anni.parkkila@kaikuhealth.com>
2022-06-21 14:30:37 +0300

    ts configs

---
+++ js/app.js
@@ -0,0 +1,7 @@
+"use strict";
+function startGame() {
+    var messagesElement = document.getElementById('messages');
+    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+}
+document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
