c692c738cd6d8d40a6f25f3cc0b011322dcaa402
BenJarrett <b.cl.jarrett@gmail.com>
2023-01-09 09:04:12 -0600

    Commit

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,8 @@
 function startGame() {
   // starting new game 
-}
+  var messagesElement = document.getElementById('messages');
+// reference to the messages div inside the index.html file // 
+  messagesElement.innerText = 'Welcome to MultiMat! Starting a new game... ';
+// Assigns a string to it's innerText property so that it will appear on the screen // 
+  }     
   
